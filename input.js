8824f603f4841eddc2c9e798742ffa96b036d093
duducesconetto <cesconettoedu@gmail.com>
2022-06-14 19:56:47 +0000

    keep the commands wait to play send a order to than send to client

--- input.js
+++ input.js
@@ -21,7 +21,18 @@ const handleUserInput = function (data) {
     connection.write("Move: down");
   } else if (data === 'd') {
     connection.write("Move: right");
+     
+     //send a mensage to others if you press the letter in the game
+  } else if (data === 'r') {  
+    connection.write("Say: let's goooo");
+  } else if (data === 't') {  
+    connection.write("Say: it's growing up");
+  } else if (data === 'y') {  
+    connection.write("Say: almost there");
+  } else if (data === 'e') {  
+    connection.write("Say: It's Ed here");
   };
+
 }
 
 
