5fbabb541a56c687696085499276c29751362ba0
Eduardo Cesconetto <99515575+cesconettoedu@users.noreply.github.com>
2022-06-16 15:42:02 -0400

    Delete stretch directory

--- stretch/play.js
+++
@@ -1,9 +0,0 @@
-const connect = require("./client");
-const setupInput = require("./input");
-
-
-//console.log("Connecting ...");
-const conn = connect();
-setupInput(conn); // receive the output from connect function in client.js and pass to setupinput at input.js
-
-
