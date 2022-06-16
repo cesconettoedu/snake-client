d6aaf15cb2948d558502c5d681a383a5708937b0
duducesconetto <cesconettoedu@gmail.com>
2022-06-16 05:45:41 +0000

    beta version, still working on improvements

---
+++ stretch/play.js
@@ -0,0 +1,9 @@
+const connect = require("./client");
+const setupInput = require("./input");
+
+
+//console.log("Connecting ...");
+const conn = connect();
+setupInput(conn); // receive the output from connect function in client.js and pass to setupinput at input.js
+
+
